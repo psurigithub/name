2c5d7f453fff2bc278924f4b35e70c25e7db4b69
Suri Babu Parepalli <suribabuparepalli04@gmail.com>
2023-02-24 07:17:09 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import Header from './components/Header'
+
+const App = () => <Header />
+
+export default App
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+// Write your JS code here
+import {Component} from 'react'
+
+class Header extends Component {
+  render() {
+    return <h1>Suri</h1>
+  }
+}
+
+export default Header
